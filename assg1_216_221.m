5360d6d41ca3e69804c378a9177595bda3157623
16CO221 <16co221.govind@nitk.edu.in>
2018-02-23 19:22:39 +0530

    Trellis

--- assg1_216_221.m
+++ assg1_216_221.m
@@ -8,7 +8,7 @@ index=1;
 output=zeros(1,2);
 while(sum(states)>0 || size(input,2)>0)
     
-    states = circshift(states,1);
+    states = circshift(states,1);   
     if size(input,2)
         states(1)=input(end);    
     else
@@ -21,6 +21,36 @@ while(sum(states)>0 || size(input,2)>0)
 
     g1=mod(sum(states([5 6 8 9 10])),2)
     g2=mod(sum(states([3 4 5 7 8 10])),2)
+
+    
+    output(index)=g1;
+    output(index+1)=g2;
+    
+    index=index+2;
+    
+end
+
+
+input=[1 0 1 1];
+states=zeros(1,4);
+
+index=1;
+output=zeros(1,2);
+while(sum(states)>0 || size(input,2)>0)
+    
+    states = circshift(states,1);   
+    if size(input,2)
+        states(1)=input(1);    
+    else
+        states(1)=0;
+    end
+    input
+    states
+    
+    input=input(2:end);
+
+    g1=mod(sum(states([1 2 3 4])),2)
+    g2=mod(sum(states([1 2 4])),2)
 
     
     output(index)=g1;
