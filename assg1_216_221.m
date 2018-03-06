b8687b616c4f46e0759014eaaf5419588878dbb9
16CO221 <16co221.govind@nitk.edu.in>
2018-03-07 02:01:56 +0530

    Contributor 2

--- assg1_216_221.m
+++ assg1_216_221.m
@@ -6,296 +6,75 @@ global lastq;
 global encoded;
 global n;
 global s;
+global maxtime;
+global flag;
+global pathmetric;
 
+maxtime=8;
 n=4;
 s=2^(n-1);
 
 
 %INITIALIZING A QUEUE
-initializeQ()
+initializeQ();
+
+%%GIVE THE INPUT HERE
+
+input=[1 0 1 0 0 1 0 0 1];
+encoded=encoder(input);
+maxtime=size(encoded,2)/2+1;
+td=generatetrellis(td);
+initializeQ();
 
 
-td=generatetrellis(td)
 % TRELLIS DIAGRAM FOR THE CONVOLUTIONAL CODE IS NOW AVAILABLE
 %------------------------------------------------------------
 
 
-%%GIVE THE INPUT HERE
-
-input=[1 0 1 1 0 0 0];
-
-% ENCODE THE INPUT
-encoded=encoder(input);
+total=zeros(4,1);
+count=zeros(4,1);
 
 
+for k=1:4
+    for i=1:250
+    errorcode=encoded;
+    y = randsample(size(encoded,2),k)
+    errorcode(y)=~errorcode(y);
+    
+    total(k)=total(k)+1;
+    % CORRECTING ERROR CODE TO GET CODEWORD USING VITERBI
+    correctpath=viterbi(errorcode);
 
-% INTRODUCE ERRORS TO THE ENCODED WORD
-errorcode=encoded;
-errorcode(1)=1;
-errorcode(2)=0;
-errorcode(5)=1;
-errorcode(15)=0;
-errorcode(16)=0;
+    corrected=corrector(correctpath);
 
-errorcode(11)=1;
-
-%Purging the queue for reuse
-initializeQ()
-
-
-% CORRECTING ERROR CODE TO GET CODEWORD USING VITERBI
-correctpath=viterbi(errorcode);
-
-corrected=corrector(correctpath)
-encoded
-
-errorcode
-
-corrected
-%VERIFYING THE CORRECTED CODEWORD
-%COMPARING IT TO THE ENCODED WORD
-verify(corrected);
-
-
-
-
-
-
-
-%==============> FUNCTION TO INITIALIZE A QUEUE DATA STRUCTURE <=================
-%---------------- IMPLEMENTED USING GLOBAL ARRAYS --------------------------------
-function q=initializeQ()
-global firstq;
-global queue;
-global lastq;
-queue  = zeros( 50, 2 );
-firstq= 1;
-lastq  = 1;
-q=queue;
-end
-
-
-%==============> FUNCTION THAT GENERATES TRELLIS DIAGRAM <==========================
-%------------- USES A GRAPH TRAVERSAL ALGORITHM TO TRANVERSE AND ASSIGN VALUES TO EACH POINT OF THE DIAGRAM--------
-function td=generatetrellis(td)
-state=0;
-time=0;
-global s;
-
-td=zeros(s,s,4);
-for i=1:s
-    for j=1:s
-    td(i,j,1)=-1;
-    td(i,j,2)=-1;
-    td(i,j,3)=-1;
-    td(i,j,4)=-1;
+    if verify(corrected)~=1
+        count(k,1)=count(k,1)+1;
+    end
     end
 end
-enqueue(state,time);
-while notEmpty()
-    
-   [state,time]=dequeue();
-   
-   td=path(state,time,td);
-   if time+1 <s
-   enqueue(td(state+1,time+1,1),time+1);
-   enqueue(td(state+1,time+1,2),time+1);
-   end
 
-end
-end
+count
+total
 
+errorrate=sum(count)/sum(total)*100
+percent=(total-count)/total*100;
+percent=percent(:,1)
+k=[1:4];
+figure
+graph=plot(percent,'b --o')
+title('Convolutional Encoder/Decoder Viterbi Plot I')
+xlabel('Error Bits')
+ylabel('Success %')
+ylim([0 100])
 
+figure
+graph=plot(k,total,'b --o')
+title('Convolutional Encoder/Decoder Viterbi Plot II')
+xlabel('Error Bits')
+ylabel('Success %')
+ylim([0 100])
 
-% ===========> VERIFICATION BY COMPARING THE CORRECTED CODEWORD TO THE CODE WORD THAT WAS INITIALLY ENCODED
-function success = verify(corrected)
-k=1;
-global encoded;
-success=1;
-
-if size(encoded,2) ~= size(corrected,2)
-    success=0;
-    disp("Correction Unsuccessful");
-    return;
-end
-while k < size(encoded,2)
-    if corrected(k)~=encoded(k)
-        success=0;
-    end
-    k=k+1;
-end
-
-if success==1
-    disp("Correction Successful"); 
-else
-    disp("Correction Unsuccessful");
-end
-
-end
-
-
-% ===========> VITERBI DECODER <========================================
-% INITIALIZES A 2-D PATHMETRIC ARRAY
-% THE LEFT TO RIGHT TRAVERSAL OF THE PATHMETRIC ARRAY DENOTES THE HISTORY
-% OF DECODING
-
-% LAST COLUMN OF PATH METRIC ARRAY SHOWS THE FINAL PATH METRIC AFTER
-% COMPLETE TRAVERSAL
-
-function correctpath=viterbi(encoded)
-
-global pathmetric;
-global s;
-pathmetric=repmat(10000,size(encoded,2)/2,size(encoded,2)/2+1);
-global td;
-
-time=0;state=0;
-enqueue(state,time);
-
-global flag;
-flag=repmat(-1,s,size(encoded,2)/2);
-
-    while notEmpty()
-        
-    
-    [state,time]=dequeue();
-    if time> size(encoded,2)/2
-        return;
-    end
-    
-    received=[encoded(2*time+1),encoded(2*time+2)];
-    
-    disp(state +" "+ time); 
-    
-    value=binarify(td(state+1,time+1,3));
-    value=value(2:end);
-    
-    disp("Sequence Value: ");
-    disp(received);
-    
-    
-    disp("Trellis (Path Zero)");
-    disp(value);
-    
-    if time==0
-        zero=hd(received,value)
-    else
-        zero=hd(received,value)+pathmetric(state+1,time+1)
-    end
-    metricupdate(state,time,zero,0)
-    
-    
-    value=binarify(td(state+1,time+1,4));
-    value=value(2:end);
-    
-    disp("Trellis (Path One): ");
-    disp(value);
-    if time==0
-        one= hd(received,value)
-    else
-        one= hd(received,value)+pathmetric(state+1,time+1)
-    end
-    metricupdate(state,time,one,1)
-    disp(" ");
-    end
-    
-
-  % Minimum Path Metric is selected
-    x=(size(encoded,2)/2);
-    [min_v,min_i]=min(pathmetric(:,x));
-    
-    correctpath=[min_i,min_i-1]
-
-    while x>1
-        min_i=correctpath(1);
-        x=x-1;
-        correctpath=[flag(min_i+1,x),correctpath]
-    end
-    
-end
-
-    
-    % ============> CORRECTION FUNCTION <=============================
-    % FUNCTION TO DETERMININE THE CORRECTED SEQUENCE BASED ON THE CORRECT
-    % PATH GENERATED BY VITERBI ALGORITHM
-    
-    function corrected=corrector(correctpath)
-    global td;
-    k=2;
-    time=0;
-    corrected=[0,0];
-    while k< size(correctpath,2)+1
-        i=correctpath(k)
-        if k == size(correctpath,2)
-            disp("Output 0: "+td(i+1,time+1,3));
-            o=binarify(td(i+1,time+1,3));
-            o=o(2:end);
-            corrected=[corrected,o];
-            corrected=corrected(3:end);
-            return
-        end
-        
-        if td(i+1,time+1,1)==correctpath(k+1)
-           disp("Output 0: "+ td(i+1,time+1,3));
-           o=binarify(td(i+1,time+1,3));
-           o=o(2:end);
-           corrected=[corrected,o];
-        end
-        
-        if td(i+1,time+1,2)==correctpath(k+1)
-           disp("Output 1: "+ td(i+1,time+1,4));
-            o=binarify(td(i+1,time+1,4));
-            o=o(2:end);
-            corrected=[corrected,o];
-        end 
-        k=k+1;
-        time=time+1;
-        
-    end
-   
-    end
-    
-  
-    
-    
-   
-% HELPER FUNCTION FOR UPDATING METRIX MATRIX IN VITERBI ALGORITHM
-    
-function pm=metricupdate(state, time, new_metric,path)
-    
-    global pathmetric;
-    global td;
-    global flag;
-    global encoded;
-    next=td(state+1,time+1,path+1)
-    
-    if pathmetric(next+1,time+2) > new_metric
-        pathmetric(next+1,time+2)=new_metric;
-    if time+2 > size(encoded,2)/2
-        return
-    end
-        if flag(next+1,time+2)==-1
-            enqueue(next,time+1);
-            disp("Enqueued " + next+" , " + (time+1));
-        end
-        flag(next+1,time+2)=state;
-    end
- 
-    pm=pathmetric;
-    
-end 
-
-
-% ==============> 2-bit Hamming Distance Calculator <==========
-function z=hd(x,y)
-z=0;
-if x(1)~=y(1)
-    z=z+1;
-end
-if x(2)~= y(2)
-    z=z+1;
-end
-end
+errorrate
 
 
 %=======> CONVOLUTIONAL ENCODER FUNCTION <============
@@ -315,7 +94,7 @@ function encoded=encoder(input)
         input=input(2:end);
 
         g1=caluc_g1(states);
-        g2=caluc_g2(states)
+        g2=caluc_g2(states);
 
 
         encoded(index)=g1;
@@ -329,9 +108,43 @@ end
 
 
 
-% ============> UPDATER FUCNTION FOR THE TRELLIS DIAGRAM <===============
-function y=path(state,time,trellisdiag)
 
+%==============> FUNCTIONS THAT GENERATES TRELLIS DIAGRAM <==========================
+%--------------TRELLIS_FUNCTIONS
+%------------- USES A GRAPH TRAVERSAL ALGORITHM TO TRANVERSE AND ASSIGN VALUES TO EACH POINT OF THE DIAGRAM--------
+function td=generatetrellis(td)
+state=0;
+time=0;
+global s;
+global maxtime;
+td=zeros(s,maxtime,4);
+for i=1:s
+    for j=1:maxtime
+    td(i,j,1)=-1;
+    td(i,j,2)=-1;
+    td(i,j,3)=-1;
+    td(i,j,4)=-1;
+    end
+end
+enqueue(state,time);
+while notEmpty()
+    
+   [state,time]=dequeue();
+   
+   td=path(state,time,td);
+   if time+1 < maxtime
+   enqueue(td(state+1,time+1,1),time+1);
+   enqueue(td(state+1,time+1,2),time+1);
+   end
+
+end
+end
+
+
+% UPDATER FUCNTION FOR THE TRELLIS DIAGRAM
+function y=path(state,time,trellisdiag)
+    time;
+    state;
     y=trellisdiag;
     [zero,one]=nextState(state);
     [zero_o,one_o]=output(state);
@@ -342,32 +155,267 @@ function y=path(state,time,trellisdiag)
     
 end
 
-% =======> DETERMINES THE OUTPUT OF A STATE FOR 0 AND 1 INPUT <========
+% DETERMINES THE OUTPUT OF A STATE FOR 0 AND 1 INPUT
 function [zero,one] = output(i)
- states=binarify(i);
- states = [0, states];
- zero=[caluc_g1(states),caluc_g2(states);];
- zero=decify(zero);
+ global n;
+ states=de2bi(i,'left-msb');
+ extra=n-size(states,2);
+ extraO=zeros(1,extra);
+ states = [extraO, states];
+ zero=[caluc_g1(states),caluc_g2(states)];
+ zero=bi2de(zero,'left-msb');
  
  states(1)=1;
  one=[caluc_g1(states),caluc_g2(states)];
- one=decify(one);
+ one=bi2de(one,'left-msb');
  
 end
 
 % DETERMINES THE NEXT STATE FOR 0 AND 1 INPUT
 
 function [zero,one] = nextState(i)
- states=binarify(i);
+ global n;
+ states=de2bi(i,'left-msb');
+ extra=n-1-size(states,2);
+ extraO=zeros(1,extra);
+ states = [extraO, states];
  states = circshift(states,1);
+ 
  states(1)=0;
- zero=decify(states);
+ zero=bi2de(states,'left-msb');
  states(1)=1;
- one=decify(states);
+ one=bi2de(states,'left-msb');
 end
 
-% FUNCTION TO CONVERT BINARY TO DECIMAL
 
+
+function g1=caluc_g1(states)
+g1=mod(sum(states([1 2 3 4])),2);
+end
+
+function g2=caluc_g2(states)
+g2=mod(sum(states([1 2 4])),2);
+end
+
+%{
+
+function g1=caluc_g1(states)
+g1=mod(sum(states([5 6 8 9 10])),2);
+end
+
+function g2=caluc_g2(states)
+g2=mod(sum(states([3 4 5 7 8 10])),2);
+end
+%}
+
+
+
+
+
+
+
+
+% ===========> VITERBI DECODER <========================================
+% INITIALIZES A 2-D PATHMETRIC ARRAY
+% THE LEFT TO RIGHT TRAVERSAL OF THE PATHMETRIC ARRAY DENOTES THE HISTORY
+% OF DECODING
+
+% LAST COLUMN OF PATH METRIC ARRAY SHOWS THE FINAL PATH METRIC AFTER
+% COMPLETE TRAVERSAL
+
+function correctpath=viterbi(encoded)
+
+global pathmetric;
+global s;
+global maxtime;
+pathmetric=repmat(10000,s,maxtime);
+global td;
+
+time=0;state=0;
+enqueue(state,time);
+
+global flag;
+flag=repmat(-1,s,maxtime);
+
+    while notEmpty()
+        
+    
+    [state,time]=dequeue();
+    if time >= maxtime
+        return;
+    end
+    
+    received=[encoded(2*time+1),encoded(2*time+2)];
+    
+    
+    value=de2bi(td(state+1,time+1,3), 'left-msb');
+    if size(value,2) == 1
+        value=[0,value];
+    end
+    
+    if time==0
+        zero=hd(received,value);
+    else
+        zero=hd(received,value)+pathmetric(state+1,time+1);
+    end
+    metricupdate(state,time,zero,0);
+    
+    
+    value=de2bi(td(state+1,time+1,4), 'left-msb');
+    if size(value,2) == 1
+        value=[0,value];
+    end
+    
+
+    if time==0
+        one= hd(received,value);
+    else
+        one= hd(received,value)+pathmetric(state+1,time+1);
+    end
+    metricupdate(state,time,one,1);
+ 
+    end
+    
+
+  % Minimum Path Metric is selected
+    x=(size(encoded,2)/2);
+    [min_v,min_i]=min(pathmetric(:,x));
+    
+    correctpath=[min_i,min_i-1];
+
+    while x>1
+        min_i=correctpath(1)
+        x=x-1
+        if min_i+1 > s
+            return
+        end 
+        correctpath=[flag(min_i+1,x),correctpath]
+    end
+   
+    
+end
+
+
+  
+% HELPER FUNCTION FOR UPDATING METRIX MATRIX IN VITERBI ALGORITHM
+    
+function pm=metricupdate(state, time, new_metric,path)
+    
+    global pathmetric;
+    global td;
+    global flag;
+    global encoded;
+    next=td(state+1,time+1,path+1);
+    
+    if pathmetric(next+1,time+2) > new_metric
+        pathmetric(next+1,time+2)=new_metric;
+    if time+2 > size(encoded,2)/2
+        return
+    end
+        if flag(next+1,time+2)==-1
+            enqueue(next,time+1);
+            
+        end
+        flag(next+1,time+2)=state;
+    end
+ 
+    pm=pathmetric;
+    
+end 
+    
+    % ============> CORRECTION FUNCTION <=============================
+    % FUNCTION TO DETERMININE THE CORRECTED SEQUENCE BASED ON THE CORRECT
+    % PATH GENERATED BY VITERBI ALGORITHM
+    
+    function corrected=corrector(correctpath)
+    global td;
+    k=2;
+    time=0;
+    corrected=[0,0];
+    while k< size(correctpath,2)+1
+        i=correctpath(k);
+        if k == size(correctpath,2)
+        
+            o=binarify(td(i+1,time+1,3));
+            o=o(2:end);
+            corrected=[corrected,o];
+            corrected=corrected(3:end);
+            return
+        end
+        
+        if td(i+1,time+1,1)==correctpath(k+1)
+      
+           o=binarify(td(i+1,time+1,3));
+           o=o(2:end);
+           corrected=[corrected,o];
+        end
+        
+        if td(i+1,time+1,2)==correctpath(k+1)
+     
+            o=binarify(td(i+1,time+1,4));
+            o=o(2:end);
+            corrected=[corrected,o];
+        end 
+        k=k+1;
+        time=time+1;
+        
+    end
+   
+    end
+    
+  
+    
+    
+
+% ===========> VERIFICATION BY COMPARING THE CORRECTED CODEWORD TO THE CODE WORD THAT WAS INITIALLY ENCODED
+function success = verify(corrected)
+k=1;
+global encoded;
+success=1;
+
+if size(encoded,2) ~= size(corrected,2)
+    success=0;
+    return;
+end
+while k < size(encoded,2)
+    if corrected(k)~=encoded(k)
+        success=0;
+    end
+    k=k+1;
+end
+
+%if success==1
+    %disp("Correction Successful"); 
+%else
+    %disp("Correction Unsuccessful");
+%end
+
+end
+ 
+
+
+% ==============> 2-bit Hamming Distance Calculator <==========
+function z=hd(x,y)
+z=0;
+if x(1)~=y(1)
+    z=z+1;
+end
+if x(2)~= y(2)
+    z=z+1;
+end
+end
+
+
+
+
+
+
+
+
+
+
+% TYPE CONVERSION FUNCTIONS
+%=============================================================================================
  function bin=binarify(dec_nr)
 bin=zeros(1,3);
 i = 1;
@@ -391,27 +439,33 @@ end
  end
 
 
- % CHECKS IF THE QUEUE IS EMPTY
-function x=notEmpty()
-    global lastq;
-    global firstq;
-    if firstq==lastq
-        x=0;
-    else
-        x=1;
+
+
+function y=decify(x)
+    sum =0;
+    for i=1:length(x)
+        sum=sum+x(i)*2^(length(x)-i);
     end
+    y=sum;
 end
 
-% CALCULATION OF XOR FROM FLIP FLOP MEMORY UNITS
-function g1=caluc_g1(states)
-g1=mod(sum(states([1 2 3 4])),2);
+
+%=============================================================================================
+
+
+
+%==============> FUNCTIONS TO INITIALIZE A QUEUE DATA STRUCTURE <=================
+%---------------- IMPLEMENTED USING GLOBAL ARRAYS --------------------------------
+function q=initializeQ()
+global firstq;
+global queue;
+global lastq;
+queue  = zeros( 100, 2 );
+firstq= 1;
+lastq  = 1;
+q=queue;
 end
 
-function g2=caluc_g2(states)
-g2=mod(sum(states([1 2 4])),2);
-end
-
-%ENQUEUE FUNCTION FOR QUEUE
 function q= enqueue(state,time)
     global lastq;
     global queue;
@@ -421,7 +475,6 @@ function q= enqueue(state,time)
     q=queue;
 end
 
-%DEQUEUE FUNCTION FOR QUEUE
 function [state,time]=dequeue()
     global firstq;
     global queue;
@@ -431,11 +484,13 @@ function [state,time]=dequeue()
 end
 
 
-% FUNCTION TO CONVERT BINARY TO DECIMAL
-function y=decify(x)
-    sum =0;
-    for i=1:length(x)
-        sum=sum+x(i)*2^(length(x)-i);
+ % CHECKS IF THE QUEUE IS EMPTY
+function x=notEmpty()
+    global lastq;
+    global firstq;
+    if firstq==lastq
+        x=0;
+    else
+        x=1;
     end
-    y=sum;
 end
